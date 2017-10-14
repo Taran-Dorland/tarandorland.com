6a961ed9363b8a1f0a8a13a114c4a6c8bf3f5d65
Taran-Dorland <tarandorland@protonmail.com>
2017-10-13 21:34:08 -0400

    MAJOR UPDATE - Modernized Assign1, added dropdown menu, fixed colours, added first JS

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+function showHeaderDiv() {
+  document.getElementById('divDropdown').style.display = "block";
+}
+
+function hideHeaderDiv() {
+  document.getElementById('divDropdown').style.display = "none";
+}
+
+function headerDivAssignments() {
+  document.getElementById('dd-button-1').innerHTML = "Assignment 1";
+  document.getElementById('dd-button-1').href = "assignment1.html"
+  document.getElementById('dd-button-2').innerHTML = "Assignment 2";
+  document.getElementById('dd-button-2').href = "assignment2.html"
+  document.getElementById('dd-button-3').innerHTML = "Assignment 3";
+  document.getElementById('dd-button-3').href = "assignment3.html"
+  document.getElementById('dd-button-4').innerHTML = "Assignment 4";
+  document.getElementById('dd-button-4').href = "assignment4.html"
+}
+
+function headerDivProjects() {
+  document.getElementById('dd-button-1').innerHTML = "Project 1";
+  document.getElementById('dd-button-2').innerHTML = "Project 2";
+  document.getElementById('dd-button-3').innerHTML = "Project 3";
+  document.getElementById('dd-button-4').innerHTML = "Project 4";
+}
