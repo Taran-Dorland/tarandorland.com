6a7974a76cf5113f9b9de8afe9e59f0717ce893a
Taran-Dorland <tarandorland@protonmail.com>
2017-10-15 21:30:05 -0400

    UPDATE - Assign2-Part:A Up, Fixed minor UI details

---
+++ js/assign2.js
@@ -0,0 +1,123 @@
+body {
+  background-color: #e0e0e0;
+  width: auto;
+  height: 100%;
+}
+
+div.page-container {
+  min-height: 100%;
+  margin-bottom: -140px;
+}
+
+div.main-container {
+  top: 0px;
+  position: relative;
+  height: 1800px;
+  z-index: 10;
+  margin: 0px 0 0 0px;
+}
+
+div.center-main-container {
+  position: absolute;
+  width: 800px;
+  height: 1600px;
+  z-index: 15;
+  left: 35%;
+  margin: 30px 0 0 -150px;
+  background: White;
+  -webkit-box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  -moz-box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
+header.main-header {
+  background-color: #ffa000;
+  position: relative;
+  top: 0px;
+  left: auto;
+  right: auto;
+  height: 65px;
+  -webkit-box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+  -moz-box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+}
+
+div.hd-dropdown-content {
+  position: relative;
+  display: none;
+  width: 800px;
+  height: 60px;
+  z-index: 15;
+  left: 35%;
+  margin: 0px 0 0 -150px;
+  background-color: #ffc107;
+  -webkit-box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+  -moz-box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+}
+
+a.dd-button {
+  text-decoration: none;
+  font-size: 110%;
+  color: White;
+  position: relative;
+  z-index: 15;
+  top: 25%;
+  padding-left: 70px;
+  font-family: "Roboto", sans-serif;
+}
+
+nav.navbar-main-top {
+  margin: 0;
+  position: relative;
+  top: 50%;
+  transform: translate(-210px, -50%);
+}
+
+@font-face {
+  font-family: "Roboto";
+  src: url(https://fonts.googleapis.com/css?family=Roboto) format("truetype");
+}
+
+a.hd-button {
+  text-decoration: none;
+  font-size: 115%;
+  color: White;
+  position: relative;
+  z-index: 15;
+  left: 35%;
+  padding-right: 20px;
+  font-family: "Roboto", sans-serif;
+}
+
+a.hd-button:hover {
+  color: #eeeeee;
+}
+
+div.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: White;
+  min-width: 160px;
+  padding: 12px 16px;
+  z-index: 1;
+}
+
+a.home-button {
+  text-decoration: none;
+  position: relative;
+  font-family: "Roboto", sans-serif;
+  font-size: 175%;
+  color: White;
+  padding-left: 275px;
+  margin-right: -325px;
+}
+
+footer.main-footer {
+  background-color: #424242;
+  position: relative;
+  height: 140px;
+  -webkit-box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  -moz-box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
