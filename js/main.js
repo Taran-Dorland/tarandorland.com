9294ec7a7b2a9c0a495edaceb2db8f487d090b17
Taran-Dorland <tarandorland@protonmail.com>
2017-10-17 15:21:42 -0400

    UPDATE - Fixed file extensions in URL

--- js/main.js
+++ js/main.js
@@ -9,13 +9,13 @@ function hideHeaderDiv() {
 
 function headerDivAssignments() {
   document.getElementById('dd-button-1').innerHTML = "Assignment 1";
-  document.getElementById('dd-button-1').href = "assignment1.html"
+  document.getElementById('dd-button-1').href = "assignment1"
   document.getElementById('dd-button-2').innerHTML = "Assignment 2";
-  document.getElementById('dd-button-2').href = "assignment2.html"
+  document.getElementById('dd-button-2').href = "assignment2"
   document.getElementById('dd-button-3').innerHTML = "Assignment 3";
-  document.getElementById('dd-button-3').href = "assignment3.html"
+  document.getElementById('dd-button-3').href = "assignment1"
   document.getElementById('dd-button-4').innerHTML = "Assignment 4";
-  document.getElementById('dd-button-4').href = "assignment4.html"
+  document.getElementById('dd-button-4').href = "assignment1"
 }
 
 function headerDivProjects() {
