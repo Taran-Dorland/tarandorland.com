5f68f93083dfd1de90bf9d9c9fee32ec6c00602b
Runtness <tarandorland@protonmail.com>
2018-09-27 15:33:14 -0400

    Fixing merge issues

---
+++ tv.tarandorland.com/public_html/ttv-emb/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <body className="App-body">
+          <div className="player-main">
+            
+          </div>
+          <div className="chat-main">
+          
+          </div>
+        </body>
+      </div>
+    );
+  }
+}
+
+export default App;
