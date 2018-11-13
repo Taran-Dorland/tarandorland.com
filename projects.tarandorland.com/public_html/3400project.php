a68ccf9d8b6dd8d3274424513cbd327795575399
Runtness <tarandorland@protonmail.com>
2018-11-13 13:13:16 -0500

    Added php page for 3400 project

---
+++ projects.tarandorland.com/public_html/3400project.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>COIS-3400 Project</title>
+
+    <!-- CSS -->
+    <link href="css/3400project.css" rel="stylesheet">
+    
+</head>
+
+<body>
+
+    <div class="main-div">
+        <p>Your PHP file is working :)</p>
+        <div class="div-pasta">
+            <p>
+            ░░░░░░░░▄▄▄▀▀▀▄▄███▄░░░░░░░░░░░░░░
+            ░░░░░▄▀▀░░░░░░░▐░▀██▌░░░░░░░░░░░░░
+            ░░░▄▀░░░░▄▄███░▌▀▀░▀█░░░░░░░░░░░░░
+            ░░▄█░░▄▀▀▒▒▒▒▒▄▐░░░░█▌░░░░░░░░░░░░
+            ░▐█▀▄▀▄▄▄▄▀▀▀▀▌░░░░░▐█▄░░░░░░░░░░░
+            ░▌▄▄▀▀░░░░░░░░▌░░░░▄███████▄░░░░░░
+            ░░░░░░░░░░░░░▐░░░░▐███████████▄░░░
+            ░░░░░le░░░░░░░▐░░░░▐█████████████▄
+            ░░░░toucan░░░░░░▀▄░░░▐█████████████▄ 
+            ░░░░░░has░░░░░░░░▀▄▄███████████████ 
+            ░░░░░arrived░░░░░░░░░░░░█▀██████░░
+        </p>
+        </div>
+        <a href="index.html">Return</a>
+    </div>
+
+
+
+
+
+
+</body>
+</html>
+
+<?php
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
