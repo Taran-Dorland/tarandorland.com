c0c3dea351fc8bea87ba3f4030719287abe5310f
Runtness <tarandorland@protonmail.com>
2019-01-11 12:54:58 -0500

    Added 3420 lab section

---
+++ projects.tarandorland.com/public_html/3420labs.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>COIS-3400 Project</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- CSS -->
+    <link href="css/main.css" rel="stylesheet">
+    
+</head>
+
+<body>
+
+    <div class="main-div">
+        <p>3420 Lab stuff will be displayed here:</p>
+        <div class="div-pasta">
+            <p>
+            ░░░░░░░░▄▄▄▀▀▀▄▄███▄░░░░░░░░░░░░░░
+            ░░░░░▄▀▀░░░░░░░▐░▀██▌░░░░░░░░░░░░░
+            ░░░▄▀░░░░▄▄███░▌▀▀░▀█░░░░░░░░░░░░░
+            ░░▄█░░▄▀▀▒▒▒▒▒▄▐░░░░█▌░░░░░░░░░░░░
+            ░▐█▀▄▀▄▄▄▄▀▀▀▀▌░░░░░▐█▄░░░░░░░░░░░
+            ░▌▄▄▀▀░░░░░░░░▌░░░░▄███████▄░░░░░░
+            ░░░░░░░░░░░░░▐░░░░▐███████████▄░░░
+            ░░░░░le░░░░░░░▐░░░░▐█████████████▄
+            ░░░░toucan░░░░░░▀▄░░░▐█████████████▄ 
+            ░░░░░░has░░░░░░░░▀▄▄███████████████ 
+            ░░░░░arrived░░░░░░░░░░░░█▀██████░░
+        </p>
+        </div>
+
+    </div>
+    
+    <a href="index.html">Return</a>
+
+</body>
+</html>
+
+<?php
+
+
+
+?>
