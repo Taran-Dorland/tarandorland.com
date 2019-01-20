71fe831a6fc187a69229346e09d853c7bb02f600
Runtness <tarandorland@protonmail.com>
2019-01-20 18:33:40 -0500

    Testing

--- projects.tarandorland.com/public_html/js/shopchallenge.js
+++ projects.tarandorland.com/public_html/js/shopchallenge.js
@@ -4,7 +4,7 @@ async function favouriteItem() {
 
     //Wait
     console.log('Waiting..');
-    await sleep(100);
+    await sleep(20);
 
     //Capture the URL which contains the ID of the obj to favourite
     var currentLocation = window.location.href;
@@ -17,20 +17,27 @@ async function favouriteItem() {
     var p = document.getElementById(oldId);
 
     var item = document.getElementById(oldId)
-    
+
     //Check the star
     if (item.checked == false) {
         item.checked = true;
+
+        //Clone
+        var p_prime = p.cloneNode(true);
+        p_prime.id = "rn".concat(strId);
+
+        //Add to html
+        document.getElementById('favCon').appendChild(p_prime);
+
     } else {
         item.checked = false;
+
+        //Delete this id
+        var newId = "rn".concat(strId);
+        var newElem = document.getElementById(newId);
+
+        newElem.remove();
     }
-
-    //Clone
-    var p_prime = p.cloneNode(true);
-    p_prime.id = "rn".concat(strId);
-
-    //Add to html
-    document.getElementById('favCon').appendChild(p_prime);
 
 
 
