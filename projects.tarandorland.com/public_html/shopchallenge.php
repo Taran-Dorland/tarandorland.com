3d34ba4ead01a7fcdb79c5a1437801689957ebb1
Runtness <tarandorland@protonmail.com>
2019-01-20 15:17:14 -0500

    Testing

--- projects.tarandorland.com/public_html/shopchallenge.php
+++ projects.tarandorland.com/public_html/shopchallenge.php
@@ -131,7 +131,7 @@
                             <?=$obj_out[$i] -> title?>
                         </div>
                         <div class="desc">
-                            <?php echo $obj_out[$i] -> body; ?>
+                            <?=$obj_out[$i] -> body?>
                         </div>
                     </div>
 
