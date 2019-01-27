0c3509e4e2a966a78fe44b5dfa8015231b74b1ac
Runtness <tarandorland@protonmail.com>
2019-01-26 23:45:26 -0500

    Added files for WF api page

---
+++ projects.tarandorland.com/public_html/wfinfo.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Warframe Info</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Icon Library -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Custom CSS -->
+    <link href="css/main-projects.css" rel="stylesheet">
+    
+</head>
+
+<body>
+
+    <div>
+
+    
+
+    </div>
+
+
+
+    <!-- Jquery -->
+    <script src="js/jquery.min.js"></script>
+
+    <!-- Popper -->
+    <script src="js/popper.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Custom Javascript -->
+    <script src=""></script>
+
+</body>
+</html>
+
+<?php
+
+
+
+
+?>
