e3f470794c72f9f1692df62b8965fbebd720fe87
Runtness <tarandorland@protonmail.com>
2019-05-19 13:38:30 -0400

    Switched main page

---
+++ Includes/libraries.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+?>
+
+<body>
+
+    <!-- JQuery -->
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+
+    <!-- Popper UTILS -->
+    <script src="node_modules/popper.js/dist/umd/popper-utils.min.js"></script>
+
+    <!-- Popper -->
+    <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+
+</body>
+
+<?php
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+
+
+
+
+
+    $title = "Index";
+    include "includes/header.php";
+?>
+
+<body>
+    <main>
+
+        <?php include "includes/nav.php"; ?>
+
+        
+
+
+    </main>
+
+    <?php include "includes/libraries.php"; ?>
+
+    <!-- Custom JS -->
+    <script src="includes/js/index.js"></script>
+
+</body>
+
+
+<?php
+
+
+
+
+
+
+
+
+
+?>
