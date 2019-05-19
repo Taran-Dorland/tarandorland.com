25d05004e19c45fb5920cc33400000317d3b19ba
Runtness <tarandorland@protonmail.com>
2019-05-19 13:58:46 -0400

    ..

--- index.php
+++ index.php
@@ -6,13 +6,13 @@
 
 
     $title = "Index";
-    include 'includes/header.php';
+    include '/includes/header.php';
 ?>
 
 <body>
     <main>
 
-        <?php include 'includes/nav.php'; ?>
+        <?php include '/includes/nav.php'; ?>
 
         <?php
 
@@ -22,7 +22,7 @@
 
     </main>
 
-    <?php include 'includes/libraries.php'; ?>
+    <?php include '/includes/libraries.php'; ?>
 
     <!-- Custom JS -->
     <script src="includes/js/index.js"></script>
