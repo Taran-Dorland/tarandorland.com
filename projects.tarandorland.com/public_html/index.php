71364f25fa61226a6f77c162a51f8e3ba0242ae5
Runtness <tarandorland@protonmail.com>
2019-05-19 20:25:43 -0400

    Restructuring subdomain

---
+++ projects.tarandorland.com/public_html/index.php
@@ -0,0 +1,85 @@
+<?php
+
+
+
+?>
+
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- <link rel="icon" href="../../icon/fwm.ico"> -->
+
+    <title>Project Directory</title>
+
+    <!-- Fontawesome CSS -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="includes/css/projectIndex.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <main>
+
+        <p>Placeholder page to display WIP and completed projects</p>
+
+        <div>
+            <div>
+                <a href="https://tarandorland.com">Return</a>
+            </div>
+            <div>
+                <a href="twitchlookup.html">Twitch follows lookup</a>
+            </div>
+            <div>
+                <h6>3420 Assignment 1</h6>
+                <ul>
+                    <li><a href="assign1/q1.html">Question 1</a></li>
+                    <li><a href="assign1/q2.html">Question 2</a></li>
+                    <li><a href="assign1/q3.html">Question 3</a></li>
+                    <li><a href="assign1/q4.html">Question 4</a></li>
+                </ul>
+                <h6>3420 Assignment 2</h6>
+                <ul>
+                    <li><a href="assign2/q1.php">Question 1</a></li>
+                    <li><a href="assign2/q2.php">Question 2</a></li>
+                    <li><a href="assign2/q3.php">Question 3</a></li>
+                </ul>
+                <h6>3420 Assignment 3</h6>
+                <ul>
+                    <li><a href="assign3/q1.html">Question 1</a></li>
+                    <li><a href="assign3/q3.html">Question 3</a></li>
+                </ul>
+            </div>
+            <div>
+                <a href="shopchallenge.php">Shopify Web Challenge 2019</a>
+            </div>
+            <div>
+                <a href="data.php">Warframe JSON Data</a>
+            </div>
+            <div>
+                <a href="wfinfo.php">Warframe API info</a>
+            </div>
+        </div>
+
+    </main>
+
+    <?php include '../../includes/libraries.php'; ?>
+
+    <!-- Custom JS -->
+    <script src="includes/js/projectIndex.js"></script>
+
+</body>
+
+<?php
+
+
+?>
