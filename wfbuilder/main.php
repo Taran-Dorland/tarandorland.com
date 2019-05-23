3ade7f32d647ca6bd0eb533853c0dba0c7c0d4f5
Runtness <tarandorland@protonmail.com>
2019-05-23 17:13:32 -0400

    Setup main page with a basic layout

---
+++ wfbuilder/main.php
@@ -0,0 +1,30 @@
+<?php
+
+
+
+
+
+
+    $title = "wfbuilder home";
+    include 'wfbuilder-includes/header.php';
+?>
+
+<body>
+    <main>
+
+
+
+
+    </main>
+
+    <?php include '../includes/libraries.php'; ?>
+
+    <!-- Custom JS -->
+
+
+</body>
+
+<?php
+
+
+?>
---
+++ wfbuilder/wfbuilder-includes/header.php
@@ -0,0 +1,30 @@
+<?php
+
+
+
+?>
+
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title><?=$title?></title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+
+</head>
+
+<?php
+
+
+
+
+
+?>
