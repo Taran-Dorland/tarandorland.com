d066f743ddb32265eac85236dfced2c1085fc974
Runtness <tarandorland@protonmail.com>
2019-06-01 21:48:11 -0400

    Added Mod parser to insert into the db

--- wfbuilder/load-data.php
+++ wfbuilder/load-data.php
@@ -3,37 +3,75 @@
     $JSON = file_get_contents("../node_modules/warframe-items/data/json/Mods.json");
     $obj = json_decode($JSON, TRUE);
 
-    $jsonIterator = new RecursiveIteratorIterator(new RecursiveArrayIterator($obj), RecursiveIteratorIterator::SELF_FIRST);
-
     include 'wfbuilder-includes/dblib.php';
 
     $pdo = & dbconnect();
 
+    $sql = "INSERT INTO wf_mods(Mod_Category, Mod_Name, Mod_Desc, Mod_Polarity, Mod_Base, Mod_Limit, Mod_Rarity, Mod_Image) VALUES (?,?,?,?,?,?,?,?)";
 
-    //rint_r($obj[0]);
+    $_MODNAME;
+    $_MODPOLARITY;
+    $_MODRARITY;
+    $_MODDRAIN;
+    $_MODLIMIT;
+    $_MODDESC;
+    $_MODTYPE;
+    $_MODIMG;
 
-    
-    //foreach ($jsonIterator as $key => $val) {
-        //if(!is_array($val)) {
+    foreach ($obj as $arr) {
+        foreach ($arr as $key => $val) {
+
+            //Get correct variables
+            switch ($key) {
+                case "name":
+                    $_MODNAME = $val;
+                    //echo $key . ": " . $val . "<br>";
+                    break;
+
+                case "polarity":
+                    $_MODPOLARITY = $val;
+                    //echo $key . ": " . $val . "<br>";
+                    break;
+
+                case "rarity":
+                    $_MODRARITY = $val;
+                    //echo $key . ": " . $val . "<br>";
+                    break;
+
+                case "baseDrain":
+                    $_MODDRAIN = $val;
+                    //echo $key . ": " . $val . "<br>";
+                    break;
+
+                case "fusionLimit":
+                    $_MODLIMIT = $val;
+                    //echo $key . ": " . $val . "<br>";
+                    break;
+
+                case "description":
+                    $_MODDESC = $val;
+                    //echo $key . ": " . $val . "<br>";
+                    break;
+
+                case "type":
+                    $_MODTYPE = $val;
+                    //echo $key . ": " . $val . "<br>";
+                    break;
+
+                case "imageName":
+                    $_MODIMG = $val;
+                    //echo $key . ": " . $val . "<br>";
+                    break;
+            }
             
-            
+        }
 
+        //Execute SQL
+        $stmt = $pdo -> prepare($sql);
+        $stmt -> execute([$_MODTYPE, $_MODNAME, $_MODDESC, $_MODPOLARITY, $_MODDRAIN, $_MODLIMIT, $_MODRARITY, $_MODIMG]);
 
-        //}
-    //}
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //Return
+    header("Location: main.php");
 ?>
