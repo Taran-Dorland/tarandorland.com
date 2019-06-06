41c3791ed56c28b185552668051fc7949619bcf8
Runtness <tarandorland@protonmail.com>
2019-06-05 21:15:03 -0400

    Deploying script to add to db

--- db_scripts/load-frames.php
+++ db_scripts/load-frames.php
@@ -3,37 +3,43 @@
     $JSON = file_get_contents("../node_modules/warframe-items/data/json/Warframes.json");
     $obj = json_decode($JSON, TRUE);
 
+    include 'wfbuilder/wfbuilder-includes/dblib.php';
 
-    //include 'wfbuilder/wfbuilder-includes/dblib.php';
+    $pdo = & dbconnect();
 
-    //$pdo = & dbconnect();
-
-    //$sql = "INSERT INTO wf_frames(Name, Armour, Energy, Health, Shield, Sprint_Speed, Passive_Desc, Aura_Polarity, Polarities, Wiki_Thumb, Wiki_Url) VALUES(?,?,?,?,?,?,?,?,?,?,?);";
+    $sql = "";
 
     foreach ($obj as $key => $val) {
 
+        //Convert array of polarities to a string
+        $polarities = implode(",", $val['polarities']);
+
+        //Frame contains both thumbnail and aura
         if (isset($val['wikiaThumbnail']) && isset($val['aura'])) {
-            print_r($val['name'] . $val['armor'] . $val['power'] . $val['health'] . $val['shield'] . $val['sprintSpeed'] . $val['passiveDescription'] . $val['aura'] . $val['wikiaThumbnail'] . $val['wikiaUrl'] . "<br>");
+            $sql = "INSERT INTO wf_frames(Name, Armour, Energy, Health, Shield, Sprint_Speed, Passive_Desc, Aura_Polarity, Polarities, Wiki_Thumb, Wiki_Url) VALUES(?,?,?,?,?,?,?,?,?,?,?);";
+            $stmt = $pdo -> prepare($sql);
+            $stmt -> execute([$val['name'], $val['armor'], $val['power'], $val['health'], $val['shield'], $val['sprintspeed'], $val['passivedescription'], $val['aura'], $polarities, $val['wikiaThumbnail'], $val['wikiaUrl']]);
+
+        //Frame contains aura
         } else if (!isset($val['wikiaThumbnail'])) {
-            print_r($val['name'] . $val['armor'] . $val['power'] . $val['health'] . $val['shield'] . $val['sprintSpeed'] . $val['passiveDescription'] . $val['aura'] . $val['wikiaUrl'] . "<br>");
+            $sql = "INSERT INTO wf_frames(Name, Armour, Energy, Health, Shield, Sprint_Speed, Passive_Desc, Aura_Polarity, Polarities,  Wiki_Url) VALUES(?,?,?,?,?,?,?,?,?,?);";
+            $stmt = $pdo -> prepare($sql);
+            $stmt -> execute([$val['name'], $val['armor'], $val['power'], $val['health'], $val['shield'], $val['sprintspeed'], $val['passivedescription'], $val['aura'], $polarities, $val['wikiaUrl']]);
+
+        //Frame contains thumbnail
         } else if (!isset($val['aura'])) {
-            print_r($val['name'] . $val['armor'] . $val['power'] . $val['health'] . $val['shield'] . $val['sprintSpeed'] . $val['passiveDescription'] . $val['wikiaThumbnail'] . $val['wikiaUrl'] . "<br>");
+            $sql = "INSERT INTO wf_frames(Name, Armour, Energy, Health, Shield, Sprint_Speed, Passive_Desc, Polarities, Wiki_Thumb, Wiki_Url) VALUES(?,?,?,?,?,?,?,?,?,?);";
+            $stmt = $pdo -> prepare($sql);
+            $stmt -> execute([$val['name'], $val['armor'], $val['power'], $val['health'], $val['shield'], $val['sprintspeed'], $val['passivedescription'], $polarities, $val['wikiaThumbnail'], $val['wikiaUrl']]);
+
+        //Frames contains neither thumbnail or aura
         } else if (!isset($val['aura']) && !isset($val['wikiaThumbnail'])) {
-            print_r($val['name'] . $val['armor'] . $val['power'] . $val['health'] . $val['shield'] . $val['sprintSpeed'] . $val['passiveDescription'] . $val['wikiaUrl'] . "<br>");
+            $sql = "INSERT INTO wf_frames(Name, Armour, Energy, Health, Shield, Sprint_Speed, Passive_Desc, Polarities, Wiki_Url) VALUES(?,?,?,?,?,?,?,?,?);";
+            $stmt = $pdo -> prepare($sql);
+            $stmt -> execute([$val['name'], $val['armor'], $val['power'], $val['health'], $val['shield'], $val['sprintspeed'], $val['passivedescription'], $polarities, $val['wikiaUrl']]);
         }
 
-        foreach ($val['polarities'] as $polarity) {
-
-            print_r($polarity . " ");
-        }
-
-        echo "<br>";
-
-        //$stmt = $pdo -> prepare($sql);
-        //$stmt -> execute([$val['name'], $val['armor'], $val['power'], $val['health'], $val['shield'], $val['sprintspeed'], $val['passivedescription'], $val['aura'], $val['polarities'], $val['wikiaThumbnail'], $val['wikiaUrl']]);
     }
-
-
 
 
 ?>
