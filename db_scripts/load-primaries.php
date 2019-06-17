0f310c4058a101cb7c8efb3f90ee24c86f61e9cb
Runtness <tarandorland@protonmail.com>
2019-06-17 15:34:18 -0400

    ..

--- db_scripts/load-primaries.php
+++ db_scripts/load-primaries.php
@@ -21,12 +21,12 @@
 
         if (isset($val['ammo']) && isset($val['damage'])) {
             print_r($val['name'] . $val['type'] . $val['magazineSize'] . $val['reloadTime'] . $val['ammo'] . $val['totalDamage'] . $val['damage'] . $dmgTypes . $val['damagePerSecond'] . $val['accuracy'] . $val['criticalChance'] . $val['criticalMultiplier'] . $val['procChance'] . "<br>");
-        } else if (!isset($val['ammo'])) {
-            print_r($val['name'] . $val['type'] . $val['magazineSize'] . $val['reloadTime'] . $val['totalDamage'] . $val['damage'] . $dmgTypes . $val['damagePerSecond'] . $val['accuracy'] . $val['criticalChance'] . $val['criticalMultiplier'] . $val['procChance'] . "<br>");
-        } else if (!isset($val['damage'])) {
-            print_r($val['name'] . $val['type'] . $val['magazineSize'] . $val['reloadTime'] . $val['ammo'] . $val['totalDamage'] . $dmgTypes . $val['damagePerSecond'] . $val['accuracy'] . $val['criticalChance'] . $val['criticalMultiplier'] . $val['procChance'] . "<br>");
         } else if (!isset($val['ammo']) && !isset($val['damage'])) {
             print_r($val['name'] . $val['type'] . $val['magazineSize'] . $val['reloadTime'] . $val['totalDamage'] . $dmgTypes . $val['damagePerSecond'] . $val['accuracy'] . $val['criticalChance'] . $val['criticalMultiplier'] . $val['procChance'] . "<br>");
+        } else if (!isset($val['damage'])) {
+            print_r($val['name'] . $val['type'] . $val['magazineSize'] . $val['reloadTime'] . $val['ammo'] . $val['totalDamage'] . $dmgTypes . $val['damagePerSecond'] . $val['accuracy'] . $val['criticalChance'] . $val['criticalMultiplier'] . $val['procChance'] . "<br>");
+        } else if (!isset($val['ammo'])) {
+            print_r($val['name'] . $val['type'] . $val['magazineSize'] . $val['reloadTime'] . $val['totalDamage'] . $dmgTypes . $val['damage'] . $val['damagePerSecond'] . $val['accuracy'] . $val['criticalChance'] . $val['criticalMultiplier'] . $val['procChance'] . "<br>");
         }
         
         echo $polarities . "<br>";
