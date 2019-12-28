a41ff500a631036b5d5b58eee4604a3a82015e0e
Taran <tarandorland@protonmail.com>
2019-12-28 17:09:59 -0500

    ..

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ include 'includes/header.php';
     <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
       <header class="masthead mb-auto">
         <div class="inner">
-          <h3 class="masthead-brand">Cover</h3>
+          <h3 class="masthead-brand">Taran Dorland</h3>
             <nav class="nav nav-masthead justify-content-center">
             <a class="nav-link active" href="#">Home</a>
             <a class="nav-link" href="#">Projects</a>
@@ -56,7 +56,7 @@ include 'includes/header.php';
       </header>
 
       <main role="main" class="inner cover">
-        <h1 class="cover-heading">Taran Dorland</h1>
+        <h1 class="cover-heading">This is a header.</h1>
         <p class="lead">
             This is a page.
         </p>
