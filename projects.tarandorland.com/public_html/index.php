363c2632267a3b5e3a9ed163f80a6a3798b3eec8
Taran <tarandorland@protonmail.com>
2019-12-28 16:59:53 -0500

    ..

--- projects.tarandorland.com/public_html/index.php
+++ projects.tarandorland.com/public_html/index.php
@@ -20,10 +20,10 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
     <!-- Bootstrap Core CSS -->
-    <link href="../../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="" rel="stylesheet">
 
     <!-- Custom CSS -->
-    <link href="includes/css/projectIndex.css" rel="stylesheet">
+    <link href="" rel="stylesheet">
 
 </head>
 
@@ -31,44 +31,6 @@
     <main>
 
         <p>Placeholder page to display WIP and completed projects</p>
-
-        <div>
-            <div>
-                <a href="https://tarandorland.com">Return</a>
-            </div>
-            <div>
-                <a href="twitchlookup.html">Twitch follows lookup</a>
-            </div>
-            <div>
-                <h6>3420 Assignment 1</h6>
-                <ul>
-                    <li><a href="assign1/q1.html">Question 1</a></li>
-                    <li><a href="assign1/q2.html">Question 2</a></li>
-                    <li><a href="assign1/q3.html">Question 3</a></li>
-                    <li><a href="assign1/q4.html">Question 4</a></li>
-                </ul>
-                <h6>3420 Assignment 2</h6>
-                <ul>
-                    <li><a href="assign2/q1.php">Question 1</a></li>
-                    <li><a href="assign2/q2.php">Question 2</a></li>
-                    <li><a href="assign2/q3.php">Question 3</a></li>
-                </ul>
-                <h6>3420 Assignment 3</h6>
-                <ul>
-                    <li><a href="assign3/q1.html">Question 1</a></li>
-                    <li><a href="assign3/q3.html">Question 3</a></li>
-                </ul>
-            </div>
-            <div>
-                <a href="shopchallenge.php">Shopify Web Challenge 2019</a>
-            </div>
-            <div>
-                <a href="data.php">Warframe JSON Data</a>
-            </div>
-            <div>
-                <a href="wfinfo.php">Warframe API info</a>
-            </div>
-        </div>
 
     </main>
 
