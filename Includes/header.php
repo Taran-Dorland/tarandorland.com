4dcae2a3ab2d4fdc1af645ecc2f8276e8e122875
Taran <tarandorland@protonmail.com>
2019-12-28 18:13:14 -0500

    ..

--- Includes/header.php
+++ Includes/header.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
     <!-- Bootstrap core CSS -->
-    <link href="node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <meta name="msapplication-config" content="/docs/4.4/assets/img/favicons/browserconfig.xml">
     <meta name="theme-color" content="#563d7c">
@@ -18,7 +18,7 @@
     <title><?=$title?></title>
 
     <!-- Custom styles for this template -->
-    <link href="Includes/css/cover.css" rel="stylesheet">
+    <link href="/css/cover.css" rel="stylesheet">
 
     <style>
       .bd-placeholder-img {
