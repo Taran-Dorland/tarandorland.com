8e65d9217c98fef8944abf3fb5b72b3c57108e2d
Taran <tarandorland@protonmail.com>
2019-12-29 15:24:41 -0500

    ..

--- projects.tarandorland.com/public_html/index.php
+++ projects.tarandorland.com/public_html/index.php
@@ -1,26 +1,8 @@
 <?php
 
 $title = "Project Directory";
+include 'Includes/header.php';
 ?>
-<html lang="en">
-<head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title><?=$title?></title>
-
-    <!-- Fontawesome CSS -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-
-    <!-- Bootstrap Core CSS -->
-    <link href="node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="Includes/css/projectsPage.css" rel="stylesheet">
-</head>
-
 <body>
     <main>
         <div class="container">
