839c8129b1ccb103d1862b442ca5c5bc15b843a2
Taran <tarandorland@protonmail.com>
2019-12-29 17:33:05 -0500

    Added dmu project

--- projects.tarandorland.com/public_html/index.php
+++ projects.tarandorland.com/public_html/index.php
@@ -18,14 +18,51 @@ include 'Includes/header.php';
                 <div class="col-sm-12">
                     <h2 class="cover-heading">DMU Extension Utiliy</h2>
                 </div>
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="Includes/img/dmuExtensionUtility/importpage.png" class="d-block w-100" alt="Main landing page.">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="Includes/img/dmuExtensionUtility/graph.png" class="d-block w-100" alt="Account login page.">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="Includes/img/dmuExtensionUtility/table.png" class="d-block w-100" alt="Successful connection page.">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="Includes/img/dmuExtensionUtility/table2.png" class="d-block w-100" alt="Chatting example 1.">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                    </div>
                 <div class="col-sm-8">
                 </div>
-                <div class="col-sm-4">
+                <div class="col-sm-4 info-col">
                     <p>This is where some info will go</p>
                 </div>
             </div>
             <div class="row">
-                <h2 class="cover-heading">pythonChat</h2>
+            <div class="col-sm-12">
+                    <h2 class="cover-heading">pythonChat</h2>
+                </div>
+                <div class="col-sm-8">
+                </div>
+                <div class="col-sm-4 info-col">
+                    <p>This is where some info will go</p>
+                </div>
             </div>
             <div class="row">
             <div class="col-sm-12">
@@ -37,6 +74,9 @@ include 'Includes/header.php';
                         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
                         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
                         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
                     </ol>
                     <div class="carousel-inner">
                         <div class="carousel-item active">
@@ -73,7 +113,14 @@ include 'Includes/header.php';
                 </div>
             </div>
             <div class="row">
-                <h2 class="cover-heading">tarandorland.com</h2>
+            <div class="col-sm-12">
+                    <h2 class="cover-heading">tarandorland.com</h2>
+                </div>
+                <div class="col-sm-8">
+                </div>
+                <div class="col-sm-4 info-col">
+                    <p>This is where some info will go</p>
+                </div>
             </div>
         </div>
 
