c39055930a6b07e0a3c2a302999f774642a793a2
Taran <tarandorland@protonmail.com>
2019-12-29 15:21:27 -0500

    ..

--- projects.tarandorland.com/public_html/index.php
+++ projects.tarandorland.com/public_html/index.php
@@ -15,7 +15,7 @@ $title = "Project Directory";
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
     <!-- Bootstrap Core CSS -->
-    <link href="node_modules/boostrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
     <link href="Includes/css/projectsPage.css" rel="stylesheet">
