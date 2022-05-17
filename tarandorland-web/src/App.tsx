da36fdc9aaea03d4bb8e278caadfbbde92a837fd
TaranDorland <tarandorland@protonmail.com>
2022-05-17 18:19:41 -0400

    Moving to React+Express.

---
+++ tarandorland-web/src/App.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export default class App extends Component {
+
+  state = {};
+
+
+
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
